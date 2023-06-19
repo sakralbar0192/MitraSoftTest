162ff6d0fcfc9c2ccd8c984947b1a325bedce03a
Danil <sakralbar0192@gmail.com>
2023-06-19 22:58:51 +0800

    add stack info

--- src/widgets/NavLinkItem/ui/NavLinkItem.tsx
+++ src/widgets/NavLinkItem/ui/NavLinkItem.tsx
@@ -2,23 +2,34 @@ import { ECodeExamples } from 'app/codeExamples'
 import { type FC } from 'react'
 import { NavLink } from 'react-router-dom'
 import classes from './style.module.scss'
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 
 interface INavLinkItemProps {
     to: ECodeExamples
     text: string
+    tooltipText: string,
     onClick?: () => void
 }
 
-export const NavLinkItem: FC<INavLinkItemProps> = ({ to, text, onClick }) => {
+export const NavLinkItem: FC<INavLinkItemProps> = ({ to, text, onClick, tooltipText='stack' }) => {
     return (
         <li>
-            <NavLink
-                className={ ({ isActive, isPending }) => isPending ? 'pending' : isActive ? classes.active : '' }
-                to={ 'CodeExample/' + to }
-                onClick={ onClick }
+            <OverlayTrigger
+                placement='right'
+                delay={ { show: 250, hide: 400 } }
+                overlay={ <Tooltip id='button-tooltip'>
+                    { tooltipText }
+                </Tooltip> }
             >
-                {text}
-            </NavLink>
+                <NavLink
+                    className={ ({ isActive, isPending }) => isPending ? 'pending' : isActive ? classes.active : '' }
+                    to={ 'CodeExample/' + to }
+                    onClick={ onClick }
+                >
+                    {text}
+                </NavLink>
+            </OverlayTrigger>
+
         </li>
     )
 }
